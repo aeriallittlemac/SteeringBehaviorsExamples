3c40ea0bc2bf810cd085f7d4ecfc6c9e7d39a848
professorK <jeffpk@gmail.com>
2026-02-07 14:57:06 -0500

    avoider wiorking
    class and demo

--- Assets/Demos/ObstacleAvoider/ClickAvoider.cs
+++ Assets/Demos/ObstacleAvoider/ClickAvoider.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickSeeker : MonoBehaviour
+public class ClickAvoider : MonoBehaviour
 {
     // Start is called before the first frame update
     private Seeker seeker;
-    private Breaker breaker;
+    private ObstacleAvoider avoider;
     public float breakingForce = 0;
     void Start()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         SteeringObject so = GetComponent<SteeringObject>();
         seeker = new Seeker(rb);
+        avoider = new ObstacleAvoider(gameObject);
         so.AddSteeringBehavior(seeker);
-        if (breakingForce > 0)
-        {
-            breaker = new Breaker(rb, breakingForce);
-            so.AddSteeringBehavior(breaker);
-        }
+        so.AddSteeringBehavior(avoider);
     }
 
     // Update is called once per frame
@@ -26,6 +23,7 @@ public class ClickSeeker : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
+            Debug.Log("GOT CLICK");
             Ray ray = Camera.main.ScreenPointToRay(
                 Input.mousePosition);
             RaycastHit hit;
